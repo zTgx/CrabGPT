069d4d3c733faea7fa49948506a2a90c0a56f501
zTgx <747674262@qq.com>
2025-07-23 18:55:00 +0800

    In the embedding layers, we multiply those weights by √dmodel.

--- src/embeddings.rs
+++ src/embeddings.rs
@@ -1,5 +1,4 @@
 use {
-    crate::pe::{PositionEncoding, PositionEncodingConfig},
     candle_core::{Result, Tensor},
     candle_nn::{Embedding, Module, VarBuilder, embedding},
 };
@@ -17,14 +16,13 @@ pub struct InputEmbeddingConfig {
 
 #[derive(Debug, Clone)]
 pub struct InputEmbedding {
+    config: InputEmbeddingConfig,
     token_embedding_layer: Embedding,
-    pos_encoding_layer: PositionEncoding,
+    // pos_encoding_layer: PositionEncoding,
 }
 
 impl InputEmbedding {
     pub fn new(config: InputEmbeddingConfig, vb: VarBuilder) -> Result<Self> {
-        println!("Embedding Config: {:#?}", config);
-
         let token_embedding_layer = embedding(
             config.vocab_size,
             config.embedding_dim,
@@ -35,18 +33,19 @@ impl InputEmbedding {
             token_embedding_layer.embeddings().shape()
         );
 
-        let pe_config = PositionEncodingConfig::new(
-            config.max_position_embeddings,
-            config.embedding_dim,
-            crate::pe::PositionEmbeddingType::Absolute,
-        );
-        println!("PE config: {:#?}", pe_config);
+        // let pe_config = PositionEncodingConfig::new(
+        //     config.max_position_embeddings,
+        //     config.embedding_dim,
+        //     crate::pe::PositionEmbeddingType::Absolute,
+        // );
+        // println!("PE config: {:#?}", pe_config);
 
-        let pos_encoding_layer = PositionEncoding::new(pe_config, vb.device())?;
+        // let pos_encoding_layer = PositionEncoding::new(pe_config, vb.device())?;
 
         Ok(Self {
+            config,
             token_embedding_layer,
-            pos_encoding_layer,
+            // pos_encoding_layer,
         })
     }
 }
@@ -54,27 +53,33 @@ impl InputEmbedding {
 impl Module for InputEmbedding {
     // Look up the embedding matrix through the token ID
     fn forward(&self, xs: &Tensor) -> Result<Tensor> {
-        println!("\n>>>>>>>>>>>>>>>>>>> InputEmbedding <<<<<<<<<<<<<<<<<<<<<<");
+        let dmodel_sqrt = (self.config.embedding_dim as f32).sqrt();
+        let t = Tensor::new(dmodel_sqrt, xs.device())?;
 
-        // Get the sequence length from the input tensor shape
-        let (batch_size, seq_len) = xs.dims2()?;
-        println!("BatchSize: {}, seqLen: {}", batch_size, seq_len);
+        // In the embedding layers, we multiply those weights by √dmodel.
+        self.token_embedding_layer.forward(xs)?.broadcast_mul(&t)
 
-        // Get token embeddings and positional embeddings
-        let token_embeddings = self.token_embedding_layer.forward(xs)?;
-        println!("Token embeddings shape: {:#?}", token_embeddings.shape());
+        // // Get the sequence length from the input tensor shape
+        // let (batch_size, seq_len) = xs.dims2()?;
+        // println!("BatchSize: {}, seqLen: {}", batch_size, seq_len);
 
-        let pos_embeddings = self.pos_encoding_layer.forward(seq_len)?;
-        println!("PE embeddings shape: {:#?}", pos_embeddings.shape());
+        // // Get token embeddings and positional embeddings
+        // let token_embeddings = self.token_embedding_layer.forward(xs)?;
+        // println!("Token embeddings shape: {:#?}", token_embeddings.shape());
 
-        println!(">>>>>>>>>>>>>>>>>>> InputEmbedding <<<<<<<<<<<<<<<<<<<<<<\n");
+        // // let pos_embeddings = self.pos_encoding_layer.forward(seq_len)?;
+        // // println!("PE embeddings shape: {:#?}", pos_embeddings.shape());
+
+        // println!(">>>>>>>>>>>>>>>>>>> InputEmbedding <<<<<<<<<<<<<<<<<<<<<<\n");
 
         // Positional embeddings are added to the token embedding vector to create the input embeddings for an LLM.
         // Eg. For token A
         // Token embedding: [1.0, 1.0, 1.0]
         // PE    embedding: [1.1, 1.2, 1.3]
         // Input embedding: = [1.0, 1.0, 1.0] + [1.1, 1.2, 1.3]
-        let input_embedding = token_embeddings.broadcast_add(&pos_embeddings);
-        input_embedding
+        // let input_embedding = token_embeddings.broadcast_add(&pos_embeddings);
+        // input_embedding
+
+        // Ok(token_embeddings)
     }
 }
